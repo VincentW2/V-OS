55c0e749c33081c5490d8a57118287dffe4dbcf1
hrt <qirimhangli@gmail.com>
2017-11-17 19:26:16 +0100

    .

--- kernel.c
+++ kernel.c
@@ -5,18 +5,11 @@ void main(void) //fonction chargée en premier par boot.S
     
     ktab("BOOTED!\n", 0x03);
     ktab("Aferfer OS, v0.2\n\n\n", 0x03);
-   
     ktab("gerga3 >", 0x09);
-    
-
-    char str[70];
-    int ltr = 0;
+   
 
     while(1)
     {
 	shell();
     }
 }
-
-
-
--- shell.c
+++ shell.c
@@ -1,51 +1,73 @@
+int ltr = 0;
+int ltr2 = 0;
+char na[20] = "mov eax, 100";
+char str[100] = "";
+int nb[100];
 void shell()
 {
     char loun = 0xF;
-    
-    char temp_command; //restera sous sa forme non-ascii
-    char command[70];
-
-    char str[70];
-    int ltr = 0;
-
+    char command[100];
+   
+   
     command[ltr] = tawshin();
-    temp_command = command[ltr];
-
+    
     switch(command[ltr])
     {
-    case 0x1E : ktab("Q", loun); str[ltr] = "Q"; ltr++; break;
-    case 0x30 : ktab("B", loun); str[ltr] = "B"; ltr++; break;
-    case 0x2E : ktab("C", loun); str[ltr] = "C"; ltr++; break;
-    case 0x20 : ktab("D", loun); str[ltr] = "D"; ltr++; break;
-    case 0x12 : ktab("E", loun); str[ltr] = "E"; ltr++; break;
-    case 0x21 : ktab("F", loun); str[ltr] = "F"; ltr++; break;
-    case 0x22 : ktab("G", loun); str[ltr] = "G"; ltr++; break;
-    case 0x23 : ktab("H", loun); str[ltr] = "H"; ltr++; break;
-    case 0x17 : ktab("I", loun); str[ltr] = "I"; ltr++; break;
-    case 0x24 : ktab("J", loun); str[ltr] = "J"; ltr++; break;
-    case 0x25 : ktab("K", loun); str[ltr] = "K"; ltr++; break;
-    case 0x26 : ktab("L", loun); str[ltr] = "L"; ltr++; break;
-    case 0x27 : ktab("M", loun); str[ltr] = "M"; ltr++; break;
-    case 0x31 : ktab("N", loun); str[ltr] = "N"; ltr++; break;
-    case 0x18 : ktab("O", loun); str[ltr] = "O"; ltr++; break;
-    case 0x19 : ktab("P", loun); str[ltr] = "P"; ltr++; break;
-    case 0x10 : ktab("A", loun); str[ltr] = "A"; ltr++; break;
-    case 0x13 : ktab("R", loun); str[ltr] = "R"; ltr++; break;
-    case 0x1F : ktab("S", loun); str[ltr] = "S"; ltr++; break;
-    case 0x14 : ktab("T", loun); str[ltr] = "T"; ltr++; break;
-    case 0x16 : ktab("U", loun); str[ltr] = "U"; ltr++; break;
-    case 0x2F : ktab("V", loun); str[ltr] = "V"; ltr++; break;
-    case 0x11 : ktab("Z", loun); str[ltr] = "Z"; ltr++; break;
-    case 0x2D : ktab("X", loun); str[ltr] = "X"; ltr++; break;
-    case 0x15 : ktab("Y", loun); str[ltr] = "Y"; ltr++; break;
-    case 0x2C : ktab("W", loun); str[ltr] = "W"; ltr++; break;
-    case 0x39 : ktab(" ", loun); str[ltr] = " "; ltr++; break;
-    case 0x1C:
-    
+    case 0x1E : ktab("Q", loun); str[ltr] = 'Q'; ltr++; break;
+    case 0x30 : ktab("B", loun); str[ltr] = 'B'; ltr++; break;
+    case 0x2E : ktab("C", loun); str[ltr] = 'C'; ltr++; break;
+    case 0x20 : ktab("D", loun); str[ltr] = 'D'; ltr++; break;
+    case 0x12 : ktab("E", loun); str[ltr] = 'E'; ltr++; break;
+    case 0x21 : ktab("F", loun); str[ltr] = 'F'; ltr++; break;
+    case 0x22 : ktab("G", loun); str[ltr] = 'G'; ltr++; break;
+    case 0x23 : ktab("H", loun); str[ltr] = 'H'; ltr++; break;
+    case 0x17 : ktab("I", loun); str[ltr] = 'I'; ltr++; break;
+    case 0x24 : ktab("J", loun); str[ltr] = 'J'; ltr++; break;
+    case 0x25 : ktab("K", loun); str[ltr] = 'K'; ltr++; break;
+    case 0x26 : ktab("L", loun); str[ltr] = 'L'; ltr++; break;
+    case 0x27 : ktab("M", loun); str[ltr] = 'M'; ltr++; break;
+    case 0x31 : ktab("N", loun); str[ltr] = 'N'; ltr++; break;
+    case 0x18 : ktab("O", loun); str[ltr] = 'O'; ltr++; break;
+    case 0x19 : ktab("P", loun); str[ltr] = 'P'; ltr++; break;
+    case 0x10 : ktab("A", loun); str[ltr] = 'A'; ltr++; break;
+    case 0x13 : ktab("R", loun); str[ltr] = 'R'; ltr++; break;
+    case 0x1F : ktab("S", loun); str[ltr] = 'S'; ltr++; break;
+    case 0x14 : ktab("T", loun); str[ltr] = 'T'; ltr++; break;
+    case 0x16 : ktab("U", loun); str[ltr] = 'U'; ltr++; break;
+    case 0x2F : ktab("V", loun); str[ltr] = 'V'; ltr++; break;
+    case 0x11 : ktab("Z", loun); str[ltr] = 'Z'; ltr++; break;
+    case 0x2D : ktab("X", loun); str[ltr] = 'X'; ltr++; break;
+    case 0x15 : ktab("Y", loun); str[ltr] = 'Y'; ltr++; break;
+    case 0x2C : ktab("W", loun); str[ltr] = 'W'; ltr++; break;
+    case 0x39 : ktab(" ", loun); str[ltr] = ' '; ltr++; break;
 
+    case 0x02 : ktab("1", loun); str[ltr] = '1'; ltr++; break;
+    case 0x03 : ktab("2", loun); str[ltr] = '2'; ltr++; break;
+    case 0x04 : ktab("3", loun); str[ltr] = '3'; ltr++; break;
+    case 0x05 : ktab("4", loun); str[ltr] = '4'; ltr++; break;
+    case 0x06 : ktab("5", loun); str[ltr] = '5'; ltr++; break;
+    case 0x07 : ktab("6", loun); str[ltr] = '6'; ltr++; break;
+    case 0x08 : ktab("7", loun); str[ltr] = '7'; ltr++; break;
+    case 0x09 : ktab("8", loun); str[ltr] = '8'; ltr++; break;
+    case 0x0A : ktab("9", loun); str[ltr] = '9'; ltr++; break;
+    case 0x0B : ktab("0", loun); str[ltr] = '0'; ltr++; break;
+	
+
+	
+    case 0x1C:
+
+	ktab(str, 0x03);
 	ktab("\n", 0x07);
 	ktab("gerga3 >", 0x09);
         ltr = 0;
+	int n = 0;
+	while (n < 100)
+	{
+	    str[n] = 0;
+	    n++;
+	}
+	n = 0;
+	
 	break;
 
 	
