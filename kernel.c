2866739bc2d2cc854b2d3a18c25e0973cc4f7e8c
hrt <qirimhangli@gmail.com>
2017-11-17 20:54:23 +0100

    .

--- kernel.c
+++ kernel.c
@@ -1,3 +1,4 @@
+
 void main(void) //fonction chargée en premier par boot.S
 {
     free_vram();
@@ -6,7 +7,6 @@ void main(void) //fonction chargée en premier par boot.S
     ktab("BOOTED!\n", 0x03);
     ktab("Aferfer OS, v0.2\n\n\n", 0x03);
     ktab("gerga3 >", 0x09);
-   
 
     while(1)
     {
