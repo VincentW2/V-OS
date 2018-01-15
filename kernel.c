7a22dc3ad8c64bca349704757dda94318a068fd5
hrt <qirimhangli@gmail.com>
2018-01-15 21:12:06 +0100

    .

--- kernel.c
+++ kernel.c
@@ -1,10 +1,11 @@
 void main(void) //fonction chargée en premier par boot.S
 {
+    ktab("BOOTING......", '0');
     free_vram();
     nextpage();
     
     ktab("BOOTED!\n", 0x03);
-    ktab("Aferfer OS, v0.2\n\n\n", 0x03);
+    ktab("Aferfer OS, v0.3\n\n\n", 0x03);
 
 
    
