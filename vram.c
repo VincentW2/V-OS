1c072bf0144293e87a6dc393978096f530cffc9d
Vincent Larkin <vincenttheepic@live.com>
2018-04-11 15:37:03 -0500

    Update vram.c

--- vram.c
+++ vram.c
@@ -2,7 +2,7 @@ void free_vram()
 {
     int video_ram_tmp = 0xB8000;
     while (video_ram_tmp < 0xBFFFF) {
-	*(unsigned char*)video_ram_tmp = 0x00; //insère des 0 dans la vram
+	*(unsigned char*)video_ram_tmp = 0x00; //inserts 0 in the vram
 	video_ram_tmp++;
     }
 }
