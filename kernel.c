fd9427cc5665791b581faeb7f847e73616c0b01c
Vincent Larkin <vincenttheepic@live.com>
2018-04-11 21:15:48 -0500

    Updated kernal.c to not error while making

--- kernel.c
+++ kernel.c
@@ -7,7 +7,7 @@ void main(void) //function loaded first by boot.S
     
     ktab("BOOTED!\n", 0x03);
     ktab("V-OS, v1.0 BETA\n", 0x03);
-    ktab("Do "info" for information on the Operating System!\n\n", 0x03);
+    ktab("Do ""info"" for information on the Operating System!\n\n", 0x03);
 
     while(1)
     {
