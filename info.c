4b77e901769ce6414a689746fa75509983a4a86c
Vincent Larkin <vincenttheepic@live.com>
2018-04-11 21:20:52 -0500

    Added more info

--- info.c
+++ info.c
@@ -2,7 +2,8 @@ void info()
 {
     ktab("\n");
     ktab("Info About V-OS\n");
-    ktab("V-OS, v1.0 BETA\n\n");
+    ktab("V-OS, v1.0 BETA\n");
+    ktab("Codename ""Typist""\n\n");
     ktab("Changelog\n");
     ktab("4/11/2018 - Added Info.c\n");
     ktab("4/11/2018 - Fixed Keyboard Input\n\n");
