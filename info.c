d1f7186f6df69c9d28e5f05ce85869d44386da3f
Vincent Larkin <vincenttheepic@live.com>
2018-04-11 19:53:32 -0500

    Added credits in info.

--- info.c
+++ info.c
@@ -2,7 +2,10 @@ void info()
 {
     ktab("\n");
     ktab("Info About V-OS\n");
-    ktab("V-OS, v0.3 BETA\n\n");
+    ktab("V-OS, v0.5 BETA\n\n");
     ktab("Changelog\n");
     ktab("4/11/2018 - Added Info.c\n\n");
+    ktab("Credits\n");
+    ktab("Original Code By Pyrmalthakon\n");
+    ktab("Rebranding, Translation, and Some Added Features by VinLark\n");
 }
