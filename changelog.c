069a869524de7b0fbf18c4277ffe7348f2e7f456
Vincent Larkin <vincenttheepic@live.com>
2018-05-16 15:07:32 -0500

    Created Changelog.c

---
+++ changelog.c
@@ -0,0 +1,8 @@
+void changelog()
+{
+  vprint("Changelog\n");
+  vprint("5/16/2018 - Added Changelog\n");
+  vprint("5/12/2018 - Added New Bootloader\n");
+  vprint("4/11/2018 - Added Info.c\n");
+  vprint("4/11/2018 - Fixed Keyboard Input\n\n");
+}
