cf5d5981240d7519da04477950a674ed6216902b
Vincent Larkin <vincenttheepic@live.com>
2018-05-16 14:36:46 -0500

    Added Linebreak Under New Chnagelog

--- info.c
+++ info.c
@@ -5,7 +5,7 @@ void info()
     vprint("V-OS, v1.1 BETA\n");
     vprint("Codename ""NewBoot""\n\n");
     vprint("Changelog\n");
-    vprint("5/12/2018 - Added New Bootloader");
+    vprint("5/12/2018 - Added New Bootloader\n");
     vprint("4/11/2018 - Added Info.c\n");
     vprint("4/11/2018 - Fixed Keyboard Input\n\n");
     vprint("Credits\n");
