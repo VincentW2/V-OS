165948fcb5d63c9dc946df2e659da705f984a40e
Vincent Larkin <vincenttheepic@live.com>
2018-05-17 09:15:41 -0500

    Fixed double line break

--- help.c
+++ help.c
@@ -3,7 +3,7 @@ void help()
     vprint("\n");
     vprint("Avalible Commands\n");
     vprint("help - shows this dialouge\n");
-    vprint("lsd - prints crazy colors\n\n");
+    vprint("lsd - prints crazy colors\n");
     vprint("info - shows info about this operating system\n");
     vprint("changelog - shows changelog of V-OS");
 }
