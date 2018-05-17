cbfcaf1bccedd476be99de6a716d98541eac9168
Vincent Larkin <vincenttheepic@live.com>
2018-05-17 09:04:10 -0500

    v1.2

--- help.c
+++ help.c
@@ -5,5 +5,5 @@ void help()
     vprint("help - shows this dialouge\n");
     vprint("lsd - prints crazy colors\n\n");
     vprint("info - shows info about this operating system\n");
-    vprint("changelog - shows changelog of V-OS")
+    vprint("changelog - shows changelog of V-OS");
 }
